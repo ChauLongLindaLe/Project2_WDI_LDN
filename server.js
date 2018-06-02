1215e951364550dcd234759c377c89dd710d7b8a
Linda Le <linchowmein@hotmail.com>
2018-06-02 09:40:54 +0100

    express is working

--- server.js
+++ server.js
@@ -4,17 +4,31 @@
 
 //express to manage the website
 const express            = require('express');
-const expressLayouts     = require('express-ejs-layouts');
-const mongoose           = require('mongoose');
-const bodyParser         = require('body-parser');
-const router = require('./config/router')
+// const expressLayouts     = require('express-ejs-layouts');
+// const session            = require('express-session');
+// const mongoose           = require('mongoose');
+// const bodyParser         = require('body-parser');
+// const router             = require('./config/router');
+// const morgan             = require('morgan');
+
 
 
 
 const app = express();
 
 
+// configure express & layouts
+// app.use(expressLayouts);
+//
+// app.use(morgan('dev'));
 
-app.use(expressLayouts);
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
+
+
+// app.use(router);
+
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`port ${PORT} is where it happens`));
