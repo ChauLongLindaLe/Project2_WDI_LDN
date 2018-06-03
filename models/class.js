b10b65ec5d7f5ef27bc5e9ce8ee48d3082aaeefb
Linda Le <linchowmein@hotmail.com>
2018-06-03 09:27:22 +0100

    refactored classSchema to include comments

--- models/class.js
+++ models/class.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose');
 
-const commentSchema = new mongoose.Schema({
-  content: String,
-  user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
-});
-
 const classSchema = new mongoose.Schema({
   name: { type: String, required: true},
   time: { type: Array, 'default': [], required: true},
   address: {type: String, required: true},
   category: {type: String, required: true},
   description: {type: String, required: true},
-  image: {type: String, required: true},
+  image: String,
   website: {type: String, required: true},
-  comments: [commentSchema]
+  comments: [{
+    comment: String,
+    creator: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+  }]
+  //owner ?
 });
 
 
